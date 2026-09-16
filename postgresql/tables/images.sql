701a0de29e8dfd67dec99cd03c3d725d399eac07
rajah <pierre.tonthat@free.fr>
2026-09-16 10:22:50 +0200

    fix images table

--- postgresql/tables/images.sql
+++ postgresql/tables/images.sql
@@ -5,15 +5,18 @@ CREATE TABLE IF NOT EXISTS badasscouncil.images
     image_id integer NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     enabled boolean DEFAULT true,
 
+    message_id integer NOT NULL,
     user_id integer NOT NULL, -- owner
     ip_address inet NOT NULL,
 
     thumbnail bytea,
     data bytea,
     
-    CONSTRAINT fk_user_id_attachments FOREIGN KEY(user_id) REFERENCES badasscouncil.users(user_id)
+    CONSTRAINT fk_message_id_id_images FOREIGN KEY(message_id) REFERENCES badasscouncil.messages(message_id),
+    CONSTRAINT fk_user_id_images FOREIGN KEY(user_id) REFERENCES badasscouncil.users(user_id)
 )
-TABLESPACE vote;
-ALTER TABLE IF EXISTS vote.productions OWNER to vote;
+TABLESPACE badasscouncil;
+ALTER TABLE IF EXISTS badasscouncil.images OWNER to badasscouncil;
 
+CREATE INDEX IF NOT EXISTS ix_images_message_id ON badasscouncil.images USING btree (message_id) TABLESPACE badasscouncil;
 CREATE INDEX IF NOT EXISTS ix_images_user_id ON badasscouncil.images USING btree (user_id) TABLESPACE badasscouncil;
