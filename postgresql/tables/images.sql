23150aa79d48d475039b4fd0499520958ad289a6
rajah <pierre.tonthat@free.fr>
2026-09-16 14:06:43 +0200

    add image recipient

--- postgresql/tables/images.sql
+++ postgresql/tables/images.sql
@@ -8,16 +8,19 @@ CREATE TABLE IF NOT EXISTS badasscouncil.images
     message_id integer NOT NULL,
     user_id integer NOT NULL, -- owner
     ip_address inet NOT NULL,
+    dest_id integer,
 
     file_name character varying(1024) COLLATE pg_catalog."default",
     thumbnail bytea,
     data bytea,
     
     CONSTRAINT fk_message_id_id_images FOREIGN KEY(message_id) REFERENCES badasscouncil.messages(message_id),
-    CONSTRAINT fk_user_id_images FOREIGN KEY(user_id) REFERENCES badasscouncil.users(user_id)
+    CONSTRAINT fk_user_id_images FOREIGN KEY(user_id) REFERENCES badasscouncil.users(user_id),
+    CONSTRAINT fk_dest_id_images FOREIGN KEY(dest_id) REFERENCES badasscouncil.users(user_id)
 )
 TABLESPACE badasscouncil;
 ALTER TABLE IF EXISTS badasscouncil.images OWNER to badasscouncil;
 
 CREATE INDEX IF NOT EXISTS ix_images_message_id ON badasscouncil.images USING btree (message_id) TABLESPACE badasscouncil;
 CREATE INDEX IF NOT EXISTS ix_images_user_id ON badasscouncil.images USING btree (user_id) TABLESPACE badasscouncil;
+CREATE INDEX IF NOT EXISTS ix_images_dest_id ON badasscouncil.images USING btree (dest_id) TABLESPACE badasscouncil;
